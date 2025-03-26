4050c78edd98a96a31dc6a3ec4e256de3e159df2
Juan <penuelajuanesteban30@gmail.com>
2025-03-26 10:19:38 -0400

    Adjusted stuff for workshop

--- client/src/App.js
+++ client/src/App.js
@@ -6,26 +6,20 @@ const App = () => {
   const [pokemon, setPokemon] = useState(null);
   const [error, setError] = useState(null);
   const [berryQuery, setBerryQuery] = useState("");
+  const [berry, setBerry] = useState(null);
   const [berryError, setBerryError] = useState(null);
 
-  const fetchPokemon = async () => {
-    // If there's no query, then there's no search.
+  const fetchPokemon = async () =>
+    {
     if (!query) return;
     setError(null);
     try
     {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query.toLowerCase()}`);
-
-      // If the pokemon is mispelled or it doesn't exist
       if (!response.ok) throw new Error("Pokémon not found");
-
-      // If the code made it here, we found a pokemon, therefore we store it as data
       const data = await response.json();
       setPokemon(data);
-
     }
-    // Other errors with the API might occur, if so,
-    // it will display this error on the console
     catch (err)
     {
       setError(err.message);
@@ -33,18 +27,11 @@ const App = () => {
     }
   };
 
-  // Fetch Berry Function
   const fetchBerry = async () => {
-    setBerryError("This function doesn't work :(")
-    // This function is empty, for now...
-  }
+    // IMPLEMENT FUNCTION HERE...
+    console.log("This function doesn't exist yet!");
+  };
 
-  // This is the UI, however, I'd like for you to check out some of the fields
-  // on the pokemon object. These are accessible thanks to the API, which has
-  // mapped all of this data for you.
-
-  // For example: pokemon.name, pokemon.stats, pokemon.sprites are all fields 
-  // available from just one search.
   return (
     <> 
     <div className="container">
@@ -54,8 +41,6 @@ const App = () => {
           type="text"
           placeholder="Enter Pokémon name"
           value={query}
-
-          // This line below sets the query to the value found in the searchbox
           onChange={(e) => setQuery(e.target.value)}
         />
         <button onClick={fetchPokemon}>Search</button>
@@ -93,9 +78,7 @@ const App = () => {
           value={berryQuery}
           onChange={(e) => setBerryQuery(e.target.value)}
         />
-      <button onClick={fetchBerry}>Search</button>
-      {berryError && <p className="error-message">{berryError}</p>}
-      {/* IMPLEMENT BERRY FUNCTION HERE */}
+        <button onClick={fetchBerry}>Search</button>
       </div>
     </div>
     </>
--- server/server.js
+++ server/server.js
@@ -42,4 +42,4 @@ app.delete("/api/users/:index", (req, res) => {
     res.json({ message: "User deleted", users });
 });
 
-app.listen(5001, () => console.log("Server started on port 5001"));
+app.listen(5002, () => console.log("Server started on port 5002"));
